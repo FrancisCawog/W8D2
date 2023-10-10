59e724c093d4e7a07b8973881bb3f521070df2a4
Francis Cawog <Franciscawog@gmail.com>
2023-10-10 17:32:29 -0400

    finished phase 3

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,56 @@
-// Your code here
+Array.prototype.myEach = function(callback) {
+    
+    for (let i =0; i < this.length; i++) {
+        callback(this[i]);
+    }
+}
+
+// function square(num) {
+//     return num * num;
+// }
+
+// function cube(num) {
+//     return num * num * num;
+// }
+
+// let arr = [1,2,3]
+// arr.myEach(square);
+// console.log(arr);
+// console.log(arr.myEach(cube));
+
+Array.prototype.myMap = function(callback) {
+    let arr = [];
+    this.myEach(function(el) {
+        x = callback(el);
+        arr.push(x);
+    });
+    return arr;
+}
+
+// let arr = [1,2,3];
+// console.log(arr.myMap(cube));
+
+// function addone(num) {
+//     return num +1;
+// }
+
+// console.log([1,2,3].myMap(addone));
+
+Array.prototype.myReduce = function(callback, initialValue) {
+    if (initialValue === null){
+        initialValue = 0
+    }
+
+    let accumulator = initialValue;
+
+    this.myEach(function(ele){
+        accumulator = callback(accumulator, ele);
+    });
+    return accumulator;
+};
+
+    function adder(accumulator, num) {
+        return accumulator + num;
+    }
+
+// console.log([1,2,3].myReduce(adder, 10));
--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
@@ -1,1 +1,29 @@
-// Your code here
+Array.prototype.bubbleSort = function(){
+    let sorted = false;
+
+    while (!sorted) {
+        sorted = true;
+        for(let i = 0; i < this.length - 1; i++){
+            if (this[i] > this[i+1]){
+                [this[i],this[i+1]] = [this[i+1], this[i]];
+                sorted = false;
+            }
+        };
+    }
+    return this;
+}
+
+// console.log([2,4,1,3].bubbleSort());
+
+String.prototype.substrings = function() {
+    let arr = [];
+
+    for(let i = 0; i < this.length; i++) {
+        for(let j = i + 1; j <= this.length; j++) {
+            arr.push(this.slice(i,j));
+        }
+    }
+    return arr;
+}
+
+console.log("Francis".substrings());
